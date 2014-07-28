f9183fba63758e4b03d5fabf80f5893621261d1e
khvilon <n@khvilon.ru>
2014-07-28 19:23:56 +0400

    routing6

--- routing.js
+++ routing.js
@@ -1,13 +1,13 @@
 OLMap.prototype.route =  function()
 {
-	this.map.cursor =  'crosshair';
+	this.map.style.cursor =  'crosshair';
 
 	var me = this;
 
 	this.map.events.register('click', this.map, function(click_event)
 	{
 		alert(click_event);
-		me.map.cursor = 'default';
+		me.map.style.cursor = 'default';
 		me.map.events.unregister('click', me.map);
 		me.routeStartSelected(35,55);
 	});
@@ -25,8 +25,8 @@ OLMap.prototype.routeStartSelected =  function(lat, lon)
 OLMap.prototype.showRadarWaiter =  function()
 {
 	var pathBack = this.hostIP + '/static/compile/js/olmap/images/radar_waiter_back.png';
-	var pathArrow = this.hostIP + '/static/compile/js/olmap/imagesradar_waiter_arrow.png';
-	var html = "<img src='"+pathBack+"' style='position:absolute; left:0px; top:0px'>";
-	html+= "<img src='"+pathArrow+"' style='position:absolute; left:50%; top:51%'>";
+	var pathArrow = this.hostIP + '/static/compile/js/olmap/images/radar_waiter_arrow.png';
+	var html = "<img src='"+pathBack+"' style='position:absolute; left:0px; top:0px; z-index:1000'>";
+	html+= "<img src='"+pathArrow+"' style='position:absolute; left:50%; top:51%; z-index:1000'>";
 	$("#"+this.divName).append(html);
 }
