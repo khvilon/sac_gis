6914183bfdc0449fb45938928c92e79038a3558c
khvilon <n@khvilon.ru>
2014-07-29 17:53:15 +0400

    routing24

--- routing.js
+++ routing.js
@@ -84,12 +84,12 @@ OLMap.prototype.showRadarWaiter =  function()
 
 	var repeats = 100;
 	var animateTo = repeats*360;
-	var duration = repeats * 24 * 1000;
+	var duration = repeats * 50 * 1000;
 	$("#radar_arrow").rotate(
 	{
 		angle:-90,
 		animateTo:animateTo,
 		duration:duration,
-		center: ["50%", "98%"],
+		center: ["50%", "97.5%"],
 	});
 }
