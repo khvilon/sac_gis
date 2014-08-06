1bde1cd77c469dc488e0414e8338d5a2e2679f4e
khvilon <n@khvilon.ru>
2014-08-06 20:13:34 +0400

    vimpel11

--- markers.js
+++ markers.js
@@ -111,23 +111,23 @@ OLMap.prototype.createRequisitionsPopupHtml =  function(attr)
 
 OLMap.prototype.createStationsPopupHtml =  function(attr)
 {
-     if(attr.processkind_code == 'BS_CREATION') attr.processkind_code = 'Строительство';
-     else attr.processkind_code = 'Модернизация';
+     if(attr.processkind_code == 'BS_CREATION') attr.processkind_code = 'РЎС‚СЂРѕРёС‚РµР»СЊСЃС‚РІРѕ';
+     else attr.processkind_code = 'РњРѕРґРµСЂРЅРёР·Р°С†РёСЏ';
     html =   '<div style="background-image: url('+attr.popup_bg_path+'); background-size: 100% 200%;';
     html += 'height: 214px; width: 230px; ">';
     html += '<div style="position: absolute; top: 25px; left: 20px; color: white; padding-top: 10px;">';
     html += '<table cellpadding = 1px cellspacing=2px width=185px border=0'+
     ' style="color: white; font-size: 12px; font-family: arial">';
-    html += '<tr><td>ГФК</td><td style="white-space: no-wrap;">' + attr.gfk + '</td></tr>';
-    html += '<tr><td>Код ЕРП</td><td style="white-space: no-wrap;">' + attr.position_code + '</td></tr>';
-    html += '<tr><td>Наименование позиции</td><td style="white-space: no-wrap;">' + attr.position_name + '</td></tr>';
-    html += '<tr><td>Тип позиции</td><td style="white-space: no-wrap;">' + attr.positiongroup_name + '</td></tr>';
-    html += '<tr><td>Широта</td><td style="white-space: no-wrap;">' + attr.position_latitude + '</td></tr>';
-    html += '<tr><td> Догота</td><td style="white-space: no-wrap;">' + attr.position_longitude + '</td></tr>';
-    html += '<tr><td>Адрес</td><td style="white-space: no-wrap;">' + attr.position_addressstring_norm + '</td></tr>';
-    html += '<tr><td>Диапазон</td><td style="white-space: no-wrap;">' + attr.bsband_band + '</td></tr>';
-    html += '<tr><td>Стандарт</td><td style="white-space: no-wrap;">' + attr.telecomstandard_code + '</td></tr>';
-    html += '<tr><td>Тип работ</td><td style="white-space: no-wrap;">' + attr.processkind_code + '</td></tr>';
+    html += '<tr><td>Р“Р¤Рљ</td><td style="white-space: no-wrap;">' + attr.gfk + '</td></tr>';
+    html += '<tr><td>РљРѕРґ Р•Р Рџ</td><td style="white-space: no-wrap;">' + attr.position_code + '</td></tr>';
+    html += '<tr><td>РќР°РёРјРµРЅРѕРІР°РЅРёРµ РїРѕР·РёС†РёРё</td><td style="white-space: no-wrap;">' + attr.position_name + '</td></tr>';
+    html += '<tr><td>РўРёРї РїРѕР·РёС†РёРё</td><td style="white-space: no-wrap;">' + attr.positiongroup_name + '</td></tr>';
+    html += '<tr><td>РЁРёСЂРѕС‚Р°</td><td style="white-space: no-wrap;">' + attr.position_latitude + '</td></tr>';
+    html += '<tr><td> Р”РѕРіРѕС‚Р°</td><td style="white-space: no-wrap;">' + attr.position_longitude + '</td></tr>';
+    html += '<tr><td>РђРґСЂРµСЃ</td><td style="white-space: no-wrap;">' + attr.position_addressstring_norm + '</td></tr>';
+    html += '<tr><td>Р”РёР°РїР°Р·РѕРЅ</td><td style="white-space: no-wrap;">' + attr.bsband_band + '</td></tr>';
+    html += '<tr><td>РЎС‚Р°РЅРґР°СЂС‚</td><td style="white-space: no-wrap;">' + attr.telecomstandard_code + '</td></tr>';
+    html += '<tr><td>РўРёРї СЂР°Р±РѕС‚</td><td style="white-space: no-wrap;">' + attr.processkind_code + '</td></tr>';
     html +=  '</table></div>';
     html +=  '<div class="close" style="position: absolute; top: 23px; right: 25px;'+
         ' " onclick="window.application.olmap.popup.destroy()"></div>';
