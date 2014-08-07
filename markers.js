c069e405c3bc9bcd4d731076a2e40275a2eed7cd
khvilon <n@khvilon.ru>
2014-08-07 19:06:47 +0400

    vimpel16

--- markers.js
+++ markers.js
@@ -121,16 +121,16 @@ OLMap.prototype.createStationsPopupHtml =  function(attr)
     html += '<div style="position: absolute; top: 35px; left: 20px; color: white; padding-top: 10px;">';
     html += '<table cellpadding = 1px cellspacing=2px width=185px border=0'+
     ' style="color: white; font-size: 12px; font-family: arial; padding: 4px">';
-    html += addRow('ГФК',attr.gfk);//'<tr><td>ГФК</td><td style="white-space: no-wrap;">' + attr.gfk + '</td></tr>';
-    html += '<tr><td>Код ЕРП</td><td style="white-space: no-wrap;">' + attr.position_code + '</td></tr>';
-    html += '<tr><td>Наименование позиции</td><td style="white-space: no-wrap;">' + attr.position_name + '</td></tr>';
-    html += '<tr><td>Тип позиции</td><td style="white-space: no-wrap;">' + attr.positiongroup_name + '</td></tr>';
-    html += '<tr><td>Широта</td><td style="white-space: no-wrap;">' + attr.position_latitude + '</td></tr>';
-    html += '<tr><td> Догота</td><td style="white-space: no-wrap;">' + attr.position_longitude + '</td></tr>';
-    html += '<tr><td>Адрес</td><td style="white-space: no-wrap;">' + attr.position_addressstring_norm + '</td></tr>';
-    html += '<tr><td>Диапазон</td><td style="white-space: no-wrap;">' + attr.bsband_band + '</td></tr>';
-    html += '<tr><td>Стандарт</td><td style="white-space: no-wrap;">' + attr.telecomstandard_code + '</td></tr>';
-    html += '<tr><td>Тип работ</td><td style="white-space: no-wrap;">' + attr.processkind_code + '</td></tr>';
+    html += addRow('ГФК',attr.position_code);//'<tr><td>ГФК</td><td style="white-space: no-wrap;">' + attr.gfk + '</td></tr>';
+    html += addRow('Код ЕРП',attr.position_name);//'<tr><td>Код ЕРП</td><td style="white-space: no-wrap;">' + attr.position_code + '</td></tr>';
+    html += addRow('Наименование позиции',attr.position_name);//'<tr><td>Наименование позиции</td><td style="white-space: no-wrap;">' + attr.position_name + '</td></tr>';
+    html += addRow('Тип позиции',attr.positiongroup_name);//'<tr><td>Тип позиции</td><td style="white-space: no-wrap;">' + attr.positiongroup_name + '</td></tr>';
+    html += addRow('Широта',attr.position_latitude);//'<tr><td>Широта</td><td style="white-space: no-wrap;">' + attr.position_latitude + '</td></tr>';
+    html += addRow('Догота',attr.position_longitude);//'<tr><td> Догота</td><td style="white-space: no-wrap;">' + attr.position_longitude + '</td></tr>';
+    html += addRow('Адрес',attr.position_addressstring_norm);//'<tr><td>Адрес</td><td style="white-space: no-wrap;">' + attr.position_addressstring_norm + '</td></tr>';
+    html += addRow('Диапазон',attr.bsband_band);//'<tr><td>Диапазон</td><td style="white-space: no-wrap;">' + attr.bsband_band + '</td></tr>';
+    html += addRow('Стандарт',attr.telecomstandard_code);//'<tr><td>Стандарт</td><td style="white-space: no-wrap;">' + attr.telecomstandard_code + '</td></tr>';
+    html += addRow('Тип работ',attr.processkind_code);//'<tr><td>Тип работ</td><td style="white-space: no-wrap;">' + attr.processkind_code + '</td></tr>';
     html +=  '</table></div>';
     html +=  '<div class="close" style="position: absolute; top: 23px; right: 25px;'+
         ' " onclick="window.application.olmap.popup.destroy()"></div>';
