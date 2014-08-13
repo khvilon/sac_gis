6cd46c1f536c9813db57e601e454a26734ee2542
khvilon <n@khvilon.ru>
2014-08-13 15:01:47 +0400

    routing92

--- routing.js
+++ routing.js
@@ -67,7 +67,7 @@ OLMap.prototype.routeLPU =  function(lat, lon, i)
         "&lang=ru-RU";
 
 
-	url = window.btoa(url);
+	url = encodeURI(url);//window.btoa(url);
    	var ajaxPath =  this.hostIP + "/arm/proxy?url=" + url;
 
 
