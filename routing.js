2a54a9f114d4106dfa0fddc956038ee3e09ae00b
khvilon <n@khvilon.ru>
2014-08-14 19:08:21 +0400

    routing128

--- routing.js
+++ routing.js
@@ -178,7 +178,8 @@ OLMap.prototype.drawPath =  function(data)
 }
 
 OLMap.prototype.drawLineSlow =  function(me, line, obj, ind)
-{	me.lpusPathToDraw--;	if(ind == obj.points.length) {if(me.allLpusPathsStarted && me.lpusPathToDraw==0)me.hideRadarWaiter();return; }	line.addPoint(obj.points[ind]);
+{	me.lpusPathToDraw--;	if(ind == obj.points.length) {if(me.allLpusPathsStarted &&
+		me.lpusPathToDraw==0)me.hideRadarWaiter();return; }	line.addPoint(obj.points[ind]);
 	me.ambulancePathLayer.redraw();
 	console.log("ii" + ind);
 	setTimeout(function(){me.drawLineSlow(me, line, obj, ind+1)}, 2);}
